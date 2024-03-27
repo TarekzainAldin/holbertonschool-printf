03748ee5d98104c57fce95d3be9a48c1c9e3389c
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-03-27 09:43:48 +0000

    correc _printf

--- _printf.c
+++ _printf.c
@@ -1,38 +1,44 @@
 #include "main.h"
-#include <stdarg.h>
-
-int _printf(const char *format, ...) {
+/**
+ * _printf - My function printf
+ * @format: string to print
+ * Return: number charcarter print
+ */
+int _printf(const char *format, ...)
+{
     va_list args;
     int i, j, count = 0, decal;
     TypeDef_CodeFormat_Fonction Tab[] = {
-        {"c", print_char}, {"s", print_string}, {"d", print_decimal}, {"i", print_decimal},
-        {"u", Unsigned}, {"%", print_unknown}, {"b", Dec_to_Bin}, {"o", Dec_to_Oct},
-        {"x", Dec_to_hex}, {"X", Dec_to_HEX}, {"r", rev_str}, {"R", ROT13},
-        {"p", print_addr}, {NULL, NULL}
-    };
-    if (format == NULL) return (-1);
-    va_start(args, format);
-    for (i = 0; format[i]; i++) {
-        if (format[i] == '%' && format[i + 1]) {
-            decal = format[i + 1] == ' ' ? 1 : 0;
-            i += 1 + decal;
-            for (j = 0; Tab[j].Spe; j++) {
-                if (format[i] == *Tab[j].Spe) {
-                    count += Tab[j].Func(args);
+{"c", print_char}, {"s", print_string}, {"%", print_unknown},
+{"d", print_decimal}, {"i", print_decimal}, {"b", Dec_to_Bin},
+{"o", Dec_to_Oct}, {"x", Dec_to_hex}, {"X", Dec_to_HEX},{"u", Unsigned} };
+	if (format == NULL)
+        return (-1);
+    va_start(args, format);/* 1st variadic argument after 'format' argument */
+    for (i = 0; format[i] != 0; i++)     /* loop on 'format' characters*/ {
+        if (format[i] == '%' && format[i + 1] != '\0') /* if character = '%' */
+        {   decal = 0;
+            if (format[i + 1] == ' ')
+                decal = 1;
+            i = i + 1 + decal; /*Skip the '%' character (optional) space */
+            for (j = 0; Tab[j].Spe != 0; j++)  /* for loop on sp */
+            {
+                if (format[i] == *Tab[j].Spe)
+                {   count  = count + Tab[j].Func(args);
                     break;
                 }
             }
-            if (!Tab[j].Spe) {
-                count += _putchar(format[i + decal - 1]);
-                count += _putchar(format[i + decal]);
+            if (Tab[j].Spe == 0)
+            {   count = count + _putchar(format[i + decal - 1]);
+                count = count + _putchar(format[i + decal]);
             }
-        } else if (format[i] == '%' && !format[i + 1]) {
-            count = -1;
-        } else {
-            count += _putchar(format[i]);
         }
+        else if (format[i] == '%' && format[i + 1] == '\0')
+            count = -1;
+        else
+            count = count + _putchar(format[i]);
     }
     va_end(args);
-    return count;
+    return (count);
 }
-
+  
