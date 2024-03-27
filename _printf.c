e373d448ce21837e0866012b72e346582d2829b1
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-03-27 10:54:57 +0000

    correct2 _printf.c

--- _printf.c
+++ _printf.c
@@ -2,20 +2,32 @@
 /**
  * _printf - My function printf
  * @format: string to print
- * Return: number charcarter print
+ * Return: number charater print
  */
 int _printf(const char *format, ...)
 {
     va_list args;
     int i, j, count = 0, decal;
-    TypeDef_CodeFormat_Fonction Tab[] = {
-        {"c", print_char}, {"s", print_string}, {"%", print_unknown},
-{"d", print_decimal}, {"i", print_decimal}, {"b", Dec_to_Bin},
-{"o", Dec_to_Oct}, {"x", Dec_to_hex}, {"X", Dec_to_HEX},{"u", Unsigned} };
-	if (format == NULL)
+    TypeDef_CodeFormat_Fonction Tab[] =
+    {
+
+        {"c", print_char},
+        {"s", print_string},
+        {"%", print_unknown},
+        {"d", print_decimal},
+        {"i", print_decimal},
+        {"b", Dec_to_Bin},
+        {"o", Dec_to_Oct},
+        {"x", Dec_to_hex},
+        {"X", Dec_to_HEX},
+        {"u", Unsigned}
+    };
+
+    if (format == NULL)
         return (-1);
-    va_start(args, format);/* 1st variadic argument after 'format' argument */
-    for (i = 0; format[i] != 0; i++)     /* loop on 'format' characters*/ {
+    va_start(args, format);   /* 1st variadic argument after 'format' argument */
+    for (i = 0; format[i] != 0; i++)     /* loop on 'format' characters*/
+    {
         if (format[i] == '%' && format[i + 1] != '\0') /* if character = '%' */
         {   decal = 0;
             if (format[i + 1] == ' ')
@@ -30,11 +42,14 @@ int _printf(const char *format, ...)
             }
             if (Tab[j].Spe == 0)
             {   count = count + _putchar(format[i + decal - 1]);
-                count = count + _putchar(format[i + decal]); } }
+                count = count + _putchar(format[i + decal]);
+            }
+        }
         else if (format[i] == '%' && format[i + 1] == '\0')
             count = -1;
         else
-            count = count + _putchar(format[i]); }
+            count = count + _putchar(format[i]);
+    }
     va_end(args);
     return (count);
 }
