762046a6f2f4293cf0d4a1baac3b6681fba42fb8
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-03-27 10:01:31 +0000

    edit the code in printf

--- _printf.c
+++ _printf.c
@@ -1,44 +1,91 @@
 #include "main.h"
+#include <stdarg.h> // Include <stdarg.h> for va_list, va_start, va_end
+
 /**
- * _printf - My function printf
- * @format: string to print
- * Return: number charcarter print
+ * _printf - Custom printf function
+ * @format: Format string
+ * Return: Number of characters printed
  */
 int _printf(const char *format, ...)
 {
     va_list args;
     int i, j, count = 0, decal;
+    
+    // Define the struct TypeDef_CodeFormat_Fonction
     TypeDef_CodeFormat_Fonction Tab[] = {
-{"c", print_char}, {"s", print_string}, {"%", print_unknown},
-{"d", print_decimal}, {"i", print_decimal}, {"b", Dec_to_Bin},
-{"o", Dec_to_Oct}, {"x", Dec_to_hex}, {"X", Dec_to_HEX},{"u", Unsigned} };
-	if (format == NULL)
+        {"c", print_char},
+        {"s", print_string},
+        {"d", print_decimal},
+        {"i", print_decimal},
+        {"u", Unsigned},
+        {"%", print_unknown},
+        {"b", Dec_to_Bin},
+        {"o", Dec_to_Oct},
+        {"x", Dec_to_hex},
+        {"X", Dec_to_HEX},
+        {"r", rev_str},
+        {"R", ROT13},
+        {"p", print_addr},
+        {NULL, NULL}
+    };
+
+    // Check if format is NULL
+    if (format == NULL)
         return (-1);
-    va_start(args, format);/* 1st variadic argument after 'format' argument */
-    for (i = 0; format[i] != 0; i++)     /* loop on 'format' characters*/ {
-        if (format[i] == '%' && format[i + 1] != '\0') /* if character = '%' */
-        {   decal = 0;
+
+    // Start variable argument list
+    va_start(args, format);
+
+    // Loop through the characters in format
+    for (i = 0; format[i] != '\0'; i++)
+    {
+        // Check if the character is '%'
+        if (format[i] == '%' && format[i + 1] != '\0')
+        {
+            // Initialize decal to 0
+            decal = 0;
+            
+            // Check if the next character is a space
             if (format[i + 1] == ' ')
                 decal = 1;
-            i = i + 1 + decal; /*Skip the '%' character (optional) space */
-            for (j = 0; Tab[j].Spe != 0; j++)  /* for loop on sp */
+            
+            // Increment i by 1 + decal
+            i += 1 + decal;
+
+            // Loop through the Tab array
+            for (j = 0; Tab[j].Spe != NULL; j++)
             {
+                // Check if the current specifier matches
                 if (format[i] == *Tab[j].Spe)
-                {   count  = count + Tab[j].Func(args);
+                {
+                    // Call the corresponding printing function
+                    count += Tab[j].Func(args);
                     break;
                 }
             }
-            if (Tab[j].Spe == 0)
-            {   count = count + _putchar(format[i + decal - 1]);
-                count = count + _putchar(format[i + decal]);
+
+            // If the specifier is not found, print the characters
+            if (Tab[j].Spe == NULL)
+            {
+                count += _putchar(format[i + decal - 1]);
+                count += _putchar(format[i + decal]);
             }
         }
         else if (format[i] == '%' && format[i + 1] == '\0')
+        {
+            // If '%' is the last character, set count to -1
             count = -1;
+        }
         else
-            count = count + _putchar(format[i]);
+        {
+            // Otherwise, print the character
+            count += _putchar(format[i]);
+        }
     }
+
+    // End variable argument list
     va_end(args);
+
+    // Return the total count of characters printed
     return (count);
 }
-  
