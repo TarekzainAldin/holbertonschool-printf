8e34a0c9383a0ffbfa3bcb8885f5284aac046ae7
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-03-28 09:08:32 +0000

    correc main.c

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <limits.h>
 #include <stdio.h>
 #include "main.h"
 
@@ -17,6 +18,11 @@ int main(void)
 	len2 = printf("Let's try to printf a simple sentence.\n");
 	ui = (unsigned int)INT_MAX + 1024;
 	addr = (void *)0x7ffe637541f0;
+	len = printf("hello %s", "let go\n");
+	len2 =  _printf("hello %s", "let go\n");
+	printf("%d, %d\n", len, len2);
+	printf("length:[%d, %i]\n", len2, len2);
+	_printf("length:[%d, %i]\n", len2, len2);
 	_printf("Length:[%d, %i]\n", len, len);
 	printf("Length:[%d, %i]\n", len2, len2);
 	_printf("Negative:[%d]\n", -762534);
@@ -37,6 +43,8 @@ int main(void)
 	len2 = printf("Percent:[%%]\n");
 	_printf("Len:[%d]\n", len);
 	printf("Len:[%d]\n", len2);
+	printf("bour:[%%]\n");
+	_printf("bour:[%%]\n");
 	_printf("Unknown:[%r]\n");
 	printf("Unknown:[%r]\n");
 	return (0);
