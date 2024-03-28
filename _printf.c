abef84f080b96e483c6f0bf298204dce4c60879c
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-03-28 09:45:50 +0000

    edit onec more for the code printf.c

--- _printf.c
+++ _printf.c
@@ -6,46 +6,43 @@
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int i, j, count = 0, decal;
-    TypeDef_CodeFormat_Fonction Tab[] = {
-        {"c", print_char}, {"s", print_string},
-        {"d", print_decimal}, {"i", print_decimal}, {"u", Unsigned},
-        {"%", print_unknown}, {"b", Dec_to_Bin}, {"o", Dec_to_Oct},
-        {"x", Dec_to_hex}, {"X", Dec_to_HEX},
-        {NULL, NULL}
-    };
+va_list args;
+int i, j, count = 0, decal;
+TypeDef_CodeFormat_Fonction Tab[] = {
+	{"c", print_char}, {"s", print_string},
+	{"d", print_decimal}, {"i", print_decimal}, {"u", Unsigned},
+	{"%", print_unknown}, {"b", Dec_to_Bin}, {"o", Dec_to_Oct},
+	{"x", Dec_to_hex}, {"X", Dec_to_HEX},
+{NULL, NULL}
+};
 
-    if (format == NULL)
-        return (-1);
-    va_start(args, format);
-    for (i = 0; format[i] != '\0'; i++)
-    {
-        if (format[i] == '%' && format[i + 1] != '\0')
-        {
-            decal = 0;
-            if (format[i + 1] == ' ')
-                decal = 1;
-            i = i + 1 + decal;
-            for (j = 0; Tab[j].Spe != NULL; j++)
-            {
-                if (format[i] == *Tab[j].Spe)
-                {
-                    count += Tab[j].Func(args);
-                    break;
-                }
-            }
-            if (Tab[j].Spe == NULL)
-            {
-                count += _putchar(format[i + decal - 1]);
-                count += _putchar(format[i + decal]);
-            }
-        }
-        else if (format[i] == '%' && format[i + 1] == '\0')
-            count = -1;
-        else
-            count += _putchar(format[i]);
-    }
-    va_end(args);
-    return (count);
+if (format == NULL)
+return (-1);
+va_start(args, format);   /* 1er argument variadique apres argument 'format' */
+for (i = 0; format[i] != 0; i++)     /* boucle sur les caracteres de 'format'*/
+{
+	if (format[i] == '%' && format[i + 1] != '\0') /* si caractere = '%' */
+	{	decal = 0;
+		if (format[i + 1] == ' ')
+			decal = 1;
+		i = i + 1 + decal; /* Passer le '%' et l'eventuel [espace] */
+		for (j = 0; Tab[j].Spe != 0; j++)  /* boucle sur spé */
+		{
+			if (format[i] == *Tab[j].Spe)
+			{	count  = count + Tab[j].Func(args);
+				break;
+			}
+		}
+		if (Tab[j].Spe == 0)
+		{	count = count + _putchar(format[i + decal - 1]);
+			count = count + _putchar(format[i + decal]);
+		}
+	}
+	else if (format[i] == '%' && format[i + 1] == '\0')
+		count = -1;
+	else
+		count = count + _putchar(format[i]);
+}
+va_end(args);
+return (count);
 }
